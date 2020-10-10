2d1760d27264b750cef1763b7e9b2128927adc6b
david-rodriguez-levelup <72571654+david-rodriguez-levelup@users.noreply.github.com>
2020-10-10 13:48:18 +0200

    Initial commit

---
+++ SimpleMath.cs
@@ -0,0 +1,29 @@
+﻿namespace SimpleLibrary
+{
+
+    public static class SimpleMath
+    {
+
+        public static float Add(float factor1, float factor2)
+        {
+            return factor1 + factor2;
+        }
+
+        public static float Substract(float factor1, float factor2)
+        {
+            return factor1 - factor2;
+        }
+
+        public static float Multiply(float factor1, float factor2)
+        {
+            return factor1 * factor2;
+        }
+
+        public static float Divide(float factor1, float factor2)
+        {
+            return factor1 / factor2;
+        }
+
+    }
+
+}
